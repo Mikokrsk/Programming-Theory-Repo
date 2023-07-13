c78d85ae180cac950fe426cf7aa3eccde2ebd835
Mykola Krasko <mikokrsk@gmail.com>
2023-07-13 19:46:09 +0300

    Create Inheritance

---
+++ Assets/Scrits/Cat.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//INHERITANCE
+public class Cat : Animal
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
